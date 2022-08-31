245cdb9be73f5e7c31b1a506325b1ea7714a3665
delucis <swithinbank@gmail.com>
2022-08-31 16:26:43 +0200

    Use showcase sites endpoint to fetch URLs

--- data/sites.js
+++ data/sites.js
@@ -1,89 +1,25 @@
 module.exports = {
-  showcase: {
-    name: 'Showcase',
-    description: 'Sites from the Astro showcase',
-    options: {
-      frequency: 1380,
-    },
-    urls: [
-      'https://astro.build/',
-      'https://docs.astro.build/',
-      'https://theguardian.engineering/open-source/',
-      'https://designcember.com/',
-      'https://polinations.com/',
-      'https://firebase.blog/',
-      'https://divriots.com/',
-      'https://www.enjoyyearof.com/',
-      'https://okikio.dev/',
-      'https://www.rainsberger.ca/',
-      'https://amelitegame.com/',
-      'https://tech.trivago.com/',
-      'https://gotomi.info/',
-      'https://reindeere.ca/',
-      'https://scottkellum.com/',
-      'https://tkon.nl/',
-      'https://danramteke.com/',
-      'https://rodneylab.com/',
-      'https://publictransportforum.nz/',
-      'https://www.snezanaspasicautotrasporti.it/',
-      'https://leosvel.dev/',
-      'https://hexarolls.com/',
-      'https://www.omarpg.com/blog/',
-      'https://byteofdev.com/',
-      'https://pizzabillionaire.com/',
-      'https://www.lloydatkinson.net/',
-      'https://blog.webdevsimplified.com/',
-      'https://www.elian.codes/',
-      'https://jjaimealeman.com/',
-      'https://corset.dev/',
-      'https://design-buddy.netlify.app/',
-      'https://spreadbagelry.com/',
-      'https://vanntile.com/',
-      'https://gimbla.com/',
-      'https://www.articoliesocial.it/',
-      'https://softhardsystem.com/',
-      'https://animerdesateliers.com/',
-      'https://jadezak.com/',
-      'https://openmoji.org/',
-      'https://eriksolsen.com/',
-      'https://www.cloudnative.quest/',
-      'https://daily-dev-tips.com/',
-      'https://sklep.alablaboratoria.pl/covid/',
-      'https://myworkshops.live/',
-      'https://pukima.site/',
-      'https://meizuflux.com/',
-      'https://www.stardew.cc/',
-      'https://jhdoumbek-portfolio.netlify.app/',
-      'https://pandasuite.com/',
-      'https://killedby.tech/',
-      'https://baldbeardedbuilder.com/',
-      'https://adrianub.dev/',
-      'https://component-party.dev/',
-      'https://goldayan.netlify.app/',
-      'https://www.mostlywhat.cf/',
-      'https://shroud.email/',
-      'https://independentdocsid.com/',
-      'https://t-red.love/',
-      'https://stefenphelps.com',
-      'https://www.juliancataldo.com/',
-      'https://clebek.dev/',
-      'https://crosstownmovers.net/',
-      'http://frontendmastery.com/',
-      'https://docs.hyblocker.dev/en/',
-      'https://www.juliancataldo.com/',
-      'https://uzzieltech.co.ke/',
-      'https://linkhub.online/',
-      'https://lukealexdavis.co.uk/',
-      'https://paul.mineev.me/',
-      'https://jamesperkins.dev/',
-      'https://melonjs.org/',
-      'https://brktfldg.netlify.app/',
-      'https://hackurity.io/',
+  showcase: async () => {
+    const blocklist = new Set([
+      'https://dendauw.tech/',
+      'https://replicant.band/',
+      'https://stephen.solka.dev/',
+    ]);
 
-      /** Commented out because the tests error on these URLs. */
-      // 'https://dendauw.tech/',
-      // 'https://replicant.band/',
-      // 'https://stephen.solka.dev/',
-    ],
+    const fetch = (await import('node-fetch')).default;
+
+    /** @type {string[]} */
+    const urls = await fetch('https://astro.build/api/showcase.json')
+      .then((res) => res.json())
+      .then((json) =>
+        json.map((site) => site.url).filter((url) => !blocklist.has(url))
+      );
+
+    return {
+      name: 'Showcase',
+      description: 'Sites from the Astro showcase',
+      options: { frequency: 1380 },
+      urls,
+    };
   },
 };
