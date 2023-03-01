1e610810db83761857ec838ca5cc3e464343d257
Chris Swithinbank <swithinbank@gmail.com>
2023-03-01 19:57:53 +0200

    Validate and filter result data with Zod
    - Parse according to Zod schema and throw out bad data
    - Exclude sites that did not provide good data in most recent run

--- src/util/data.ts
+++ src/util/data.ts
@@ -1,57 +1,80 @@
-export interface TestData {
-  timestamp: number;
-  error?;
-  url: string;
-  requestedUrl?: string;
-  ranks: {
-    hundos: number;
-    performance: number;
-    accessibility: number;
-    cumulative: number;
-  };
-  lighthouse: {
-    version: string;
-    performance: number;
-    accessibility: number;
-    bestPractices: number;
-    seo: number;
-    total: number;
-    type?: string;
-  };
-  firstContentfulPaint: number;
-  firstMeaningfulPaint: number;
-  speedIndex: number;
-  largestContentfulPaint: number;
-  totalBlockingTime: number;
-  cumulativeLayoutShift: number;
-  timeToInteractive: number;
-  maxPotentialFirstInputDelay: number;
-  timeToFirstByte: number;
-  weight: {
-    summary: string;
-    total: number;
-    image: number;
-    imageCount: number;
-    script: number;
-    scriptCount: number;
-    document: number;
-    font: number;
-    fontCount: number;
-    stylesheet: number;
-    stylesheetCount: number;
-    thirdParty: number;
-    thirdPartyCount: number;
-  };
-  run: {
-    number: number;
-    total: number;
-  };
-  axe: {
-    passes: number;
-    violations: number;
-    error?: boolean;
-  };
-};
+import z from 'astro/zod';
+
+const TestError = z.object({
+  error: z.string(),
+});
+const TestResult = z.object({
+  timestamp: z.number(),
+  url: z.string().url(),
+  requestedUrl: z.string().url().optional(),
+  ranks: z.object({
+    hundos: z.number(),
+    performance: z.number(),
+    accessibility: z.number(),
+    cumulative: z.number(),
+  }),
+  lighthouse: z.object({
+    version: z.string(),
+    performance: z.number(),
+    accessibility: z.number(),
+    bestPractices: z.number(),
+    seo: z.number(),
+    total: z.number(),
+    type: z.string().optional(),
+  }),
+  firstContentfulPaint: z.number(),
+  firstMeaningfulPaint: z.number(),
+  speedIndex: z.number(),
+  largestContentfulPaint: z.number(),
+  totalBlockingTime: z.number(),
+  cumulativeLayoutShift: z.number(),
+  timeToInteractive: z.number(),
+  maxPotentialFirstInputDelay: z.number(),
+  timeToFirstByte: z.number(),
+  weight: z.object({
+    summary: z.string(),
+    total: z.number(),
+    image: z.number(),
+    imageCount: z.number(),
+    script: z.number(),
+    scriptCount: z.number(),
+    document: z.number(),
+    font: z.number(),
+    fontCount: z.number(),
+    stylesheet: z.number(),
+    stylesheetCount: z.number(),
+    thirdParty: z.number(),
+    thirdPartyCount: z.number(),
+  }),
+  run: z.object({
+    number: z.number(),
+    total: z.number(),
+  }),
+  // sidequests: z.object({
+  //   '-weight.total': z.number(),
+  //   '+weight.total': z.number(),
+  //   '-weight.document': z.number(),
+  //   '+weight.document': z.number(),
+  //   '-weight.script': z.number(),
+  //   '+weight.script': z.number(),
+  //   '-weight.image': z.number(),
+  //   '+weight.image': z.number(),
+  //   '-weight.font': z.number(),
+  //   '+weight.font': z.number(),
+  //   '+weight.fontCount': z.number(),
+  //   '-timeToFirstByte': z.number(),
+  //   '-totalBlockingTime': z.number(),
+  //   '-largestContentfulPaint': z.number(),
+  // }),
+  axe: z.object({
+    passes: z.number(),
+    violations: z.number(),
+    error: z.boolean().default(false),
+  }),
+});
+
+const TestResultOrError = z.union([TestResult, TestError]);
+export type TestData = z.infer<typeof TestResult>;
 
 /**
  * Group JSON data for each site.
@@ -64,10 +87,45 @@ function groupSites(globMap: {
   const results: { [siteHash: string]: { [date: string]: TestData } } = {};
   for (const [path, content] of Object.entries(globMap)) {
     const [_dots, _data, _results, siteHash, filename] = path.split('/');
-    if (!results[siteHash]) results[siteHash] = {};
-    results[siteHash][filename.replace(/(^date-|\.json$)/g, '')] = content;
+
+    const runTime = filename.replace(/(^date-|\.json$)/g, '');
+    const result = TestResultOrError.safeParse(content);
+    if (result.success) {
+      if (!('error' in result.data)) {
+        if (!results[siteHash]) results[siteHash] = {};
+        results[siteHash][runTime] = content;
+      }
+    } else if ('error' in result) {
+      const runMonth = new Date(Number(runTime)).toLocaleDateString('en', {
+        month: 'long',
+      });
+      console.error('Excluding bad', runMonth, 'data for', content.url, 'on');
+    } else {
+      console.error('Something unknown went wrong', result);
+    }
   }
   return Object.values(results);
+}
+
+/** Remove sites for which we don’t have good data for the most recent run. */
+function excludeOldRuns(siteGroups: { [date: string]: TestData }[]) {
+  const mostRecentRun = Math.max(
+    ...Object.values(siteGroups).flatMap(Object.keys).map(Number)
+  );
+  for (const key in siteGroups) {
+    const site = siteGroups[key];
+    const newestDate = Number(Object.keys(site).sort().pop()!);
+    const dayMs = 24 * 60 * 60 * 1000;
+    if (newestDate < mostRecentRun - dayMs) {
+      console.log(
+        'Excluding',
+        site[newestDate].url,
+        '— no data for most recent run'
+      );
+      delete siteGroups[key];
+    }
+  }
+  return siteGroups;
 }
 
 /**
@@ -79,18 +137,19 @@ function sortCumulativeScore(
   arr: { [date: string]: TestData }[]
 ): { [date: string]: TestData }[] {
   return arr.sort((a, b) => {
-    let newestKeyA = Object.keys(a).sort().pop();
-    let newestKeyB = Object.keys(b).sort().pop();
+    let newestKeyA = Object.keys(a).sort().pop()!;
+    let newestKeyB = Object.keys(b).sort().pop()!;
 
     // Lighthouse error
     // e.g. { url: 'https://mangoweb.net/', error: 'Unknown error.' }
-    if (b[newestKeyB].error && a[newestKeyA].error) {
-      return 0;
-    } else if (b[newestKeyB].error) {
-      return -1;
-    } else if (a[newestKeyA].error) {
-      return 1;
-    }
+    // Commented out because we’re ignoring errors for now.
+    // if (b[newestKeyB].error && a[newestKeyA].error) {
+    //   return 0;
+    // } else if (b[newestKeyB].error) {
+    //   return -1;
+    // } else if (a[newestKeyA].error) {
+    //   return 1;
+    // }
 
     // lower is better
     return a[newestKeyA].ranks.cumulative - b[newestKeyB].ranks.cumulative;
@@ -98,7 +157,9 @@ function sortCumulativeScore(
 }
 
 /** Get all results data, grouped and sorted. */
-export const loadResults = async () =>
+export const loadResults = () =>
   sortCumulativeScore(
-    groupSites(await import.meta.globEager('../data/results/**/*.json'))
+    excludeOldRuns(
+      groupSites(import.meta.glob('../data/results/**/*.json', { eager: true }))
+    )
   );
