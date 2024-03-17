6d17c0a183c5eefd0e1f1e951f4db594ee10537d
Chris Swithinbank <swithinbank@gmail.com>
2024-03-17 11:35:06 +0100

    Install `@astrojs/db`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.URL || 'https://hyperdrive-speedometer.netlify.app/',
+  integrations: [db()],
 });
